2c46eba8e91f41bbe1dc5aef7b119be3821970a5
Candice-schneider <94042955+Candice-schneider@users.noreply.github.com>
2022-05-04 12:18:40 +0200

    nv

--- Project/Cooking/cookingapp/views.py
+++ Project/Cooking/cookingapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+#import forms
 # Create your views here.
 
+
 def ajout(request):
-    gform = forms.RecetteForm()
-    return render(request, "cookingapp/ajout.html", {"form" : })
+    #gform = forms.RecetteForm()
+    return render(request, 'cookingapp/ajout.html'), #{"form": gform})
+
+
+def index(request):
+    return render(request, 'cookingapp/index.html')
